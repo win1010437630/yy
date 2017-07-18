317fdab77eef6353c7a4408760a67c7ccc55f830
sugerss <1915492618@qq.com>
2017-07-18 15:34:48 +0800

    two

---
+++ src/History.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './History.css';
+
+class History extends Component {
+  render() {
+    return (
+      <div className="History">
+        History
+      </div>
+    );
+  }
+}
+
+export default History;
